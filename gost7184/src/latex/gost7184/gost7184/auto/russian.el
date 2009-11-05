2c4028b27778529e5872b8c87b90cb9726886a34
Dmitry S. Kulyabov <yamadharma@gmail.com>
2009-11-05 18:57:45 +0300

    bib

---
+++ gost7184/src/latex/gost7184/gost7184/auto/russian.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook "russian"
+ (lambda ()
+    (TeX-add-symbols
+     "bbland"
+     "bbletal"
+     "bbledby"
+     "bbled"
+     "bbleds"
+     "bbledn"
+     "bblvol"
+     "bblissue"
+     "bblissuefirst"
+     "bblof"
+     "bblno"
+     "bblnofirst"
+     "bblpp"
+     "bblp"
+     "bblnump"
+     "bbltechrep"
+     "bblmthesis"
+     "bblphdthesis"
+     "bblcompiler"
+     "bblfeb")))
+
