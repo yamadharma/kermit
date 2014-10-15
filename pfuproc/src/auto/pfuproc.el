dbc83d93595073bbec7b08a64f1687deb5ebef2f
Dmitry S. Kulyabov <yamadharma@gmail.com>
2014-10-15 15:44:23 +0400

    tex4ht

--- pfuproc/src/auto/pfuproc.el
+++ pfuproc/src/auto/pfuproc.el
@@ -3,6 +3,7 @@
     (LaTeX-add-environments
      "authordescription")
     (TeX-add-symbols
+     '("country" 1)
      '("phone" 1)
      '("email" 1)
      '("authorpost" 1)
@@ -10,9 +11,11 @@
      '("authorrank" 1)
      '("authorfull" 1)
      "authorlistFontShape"
+     "PFUpaperid"
      "listofauthors")
     (TeX-run-style-hooks
      "currfile"
      "kproc10"
-     "kproc")))
+     "kproc"
+     "")))
 
