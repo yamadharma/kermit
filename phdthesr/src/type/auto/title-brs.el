db7237520b4b336fd2e8052dfce5d246add86aa4
Dmitry S. Kulyabov <yamadharma@gmail.com>
2014-12-10 17:28:11 +0300

    Редактирую титульную страницу для дипломов

---
+++ phdthesr/src/type/auto/title-brs.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "title-brs"
+ (lambda ()
+   (TeX-run-style-hooks
+    "mdwtab")
+   (TeX-add-symbols
+    "brsTable"
+    "approvedStamp"
+    "authorTable"
+    "brsTableProblemType"
+    "brsTableBalls"
+    "brsTableBallsSum"
+    "brsTableAttestInterim"
+    "brsTableAttestFinal"
+    "brsTableTotal"
+    "brsTableText"
+    "brsTablePresentation"
+    "brsTableProtection"
+    "PHDtitleDescr"
+    "brsTableCourse"
+    "PHDauthorDescr"
+    "PHDstudygroupDescr"
+    "PHDcountryDescr"
+    "PHDchiefDescr"
+    "PHDyearShort"
+    "PHDapprove"
+    "footnotesize"
+    "footnoterule")))
+
