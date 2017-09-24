0450e0b964d695e59015d367901c7b667b7076ee
Dmitry S. Kulyabov <yamadharma@gmail.com>
2017-09-24 20:22:28 +0300

    authorindex

--- kermit/src/kproc/auto/authorindex.el
+++ kermit/src/kproc/auto/authorindex.el
@@ -1,8 +1,11 @@
 (TeX-add-style-hook
  "authorindex"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("indextools" "noautomatic")))
    (TeX-run-style-hooks
     "indextools")
    (TeX-add-symbols
-    "authorindexname")))
+    "authorindexname"))
+ :latex)
 
